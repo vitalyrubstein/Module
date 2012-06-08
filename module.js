b6f4a07d78755f006d1d350e68548539a27ecd61
Vitaly Rubstein <vitaly.rubstein@gmail.com>
2012-06-08 16:30:44 +0200

    mvc - temp - not finished

--- module.js
+++ module.js
@@ -1,15 +1,63 @@
 var testModule = (function(){
 	
-	var defaults = {title: 'Hello'};
+	var defaults = {title: 'Ready'};
 	
 	var init = function(options){
 		
 		initOptions = $.extend(defaults, options);		
 		alert(initOptions['title']);
 		
-		}
+	};
 	
+	var PhotoDataServer = function () {
+		this.getPhotos = function(seconds, userID, callback){
+			$.ajax('http://176.34.237.117/json/photo_groups',{
+				data: {
+					'user_id' : userID,
+					'time' : seconds
+					},
+				cache: false,
+				dataType: 'json',
+				timeout: 30000,
+				success: function(json){callback(json);},
+				error: function(json){console.log('Network Error!');}
+			}); // end ajax
+		};
+	};
+	
+	var HomeModel = function(){
+	
+		var data;
+		
+		this.initModel = function(json){
+			data = json;
+			$('body').trigger('init');
+		};
+		
+		this.getData = function(){
+			return data;
+		};
+		
+	};
+	
+	var HomeView = function(){
+	
+		$('div#main').append('<button id="getdata">Get Data</button>');
+		$('button#getdata').on('init', function(){
+			$(this).text('Done');
+		});
+	};
+	
+	var HomeController = function(){
+		
+		$('button#getdata').click(function(){
+			PhotoDataServer.getPhotos(30000,2,HomeModel.initModel);
+		});
+	};
+		
 	return {
-		init: init
+		init: init,
+		view: HomeView,
+		controller: HomeController
 	}
 })();
